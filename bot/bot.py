f6ff9a0d35e841704d4d9d65d15abb2f4e10dfa5
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-12-20 00:14:16 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,86 @@
-from addons.utils import logger
-from helpers.media_info import *
+from signal import signal, SIGINT
+from os import path as ospath, remove as osremove, execl as osexecl
+from subprocess import run as srun, check_output
+from psutil import disk_usage, cpu_percent, swap_memory, cpu_count, virtual_memory, net_io_counters, boot_time
+from time import time
+from sys import executable
+
+from .addons.utils import logger
+from .helpers.media_info import *
 import os
-from messages.creator import *
+from .messages.creator import *
 from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-dest = "telegramMusic/"
+dest = "bot/telegramMusic/"
+
+from bot import bot, dispatcher, updater, botStartTime, Interval, app, main_loop
+from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage, sendLogFile
 
 TOKEN = '5595298904:AAExEMcbyKGA3cBdIECmFB-AD55Zx8L0uOM'
 
+def stats(update, context):
+    if ospath.exists('.git'):
+        last_commit = check_output(["git log -1 --date=short --pretty=format:'%cd <b>From</b> %cr'"], shell=True).decode()
+    else:
+    currentTime = get_readable_time(time() - botStartTime)
+    osUptime = get_readable_time(time() - boot_time())
+    total, used, free, disk= disk_usage('/')
+    total = get_readable_file_size(total)
+    used = get_readable_file_size(used)
+    free = get_readable_file_size(free)
+    sent = get_readable_file_size(net_io_counters().bytes_sent)
+    recv = get_readable_file_size(net_io_counters().bytes_recv)
+    cpuUsage = cpu_percent(interval=0.5)
+    p_core = cpu_count(logical=False)
+    t_core = cpu_count(logical=True)
+    swap = swap_memory()
+    swap_p = swap.percent
+    swap_t = get_readable_file_size(swap.total)
+    memory = virtual_memory()
+    mem_p = memory.percent
+    mem_t = get_readable_file_size(memory.total)
+    mem_a = get_readable_file_size(memory.available)
+    mem_u = get_readable_file_size(memory.used)
+    stats = f'<b>╭──「⭕️ BOT STATISTICS ⭕️」</b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>├  ⏰ Bot Uptime : {currentTime}</b>\n' \
+            f'<b>├  💾 Total Disk Space : {total}</b>\n' \
+            f'<b>├  📀 Total Used Space : {used}</b>\n' \
+            f'<b>├  💿 Total Free Space : {free}</b>\n' \
+            f'<b>├  🔼 Total Upload : {sent}</b>\n' \
+            f'<b>├  🔽 Total Download : {recv}</b>\n' \
+            f'<b>├  🖥️ CPU : {cpuUsage}%</b>\n' \
+            f'<b>├  🎮 RAM : {mem_p}%</b>\n' \
+            f'<b>├  💽 DISK : {disk}%</b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>╰──「 🚸 @sk_mass_king 🚸 」</b>'
+    sendMessage(stats, context.bot, update.message)
+    
+def restart(update, context):
+    restart_message = sendMessage("Restarting...", context.bot, update.message)
+    if Interval:
+        Interval[0].cancel()
+    clean_all()
+    srun(["pkill", "-f", "gunicorn|aria2c|qbittorrent-nox"])
+    srun(["python3", "update.py"])
+    with open(".restartmsg", "w") as f:
+        f.truncate(0)
+        f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
+    osexecl(executable, executable, "-m", "bot")
+    
+    
+def ping(update, context):
+    start_time = int(round(time() * 1000))
+    reply = sendMessage("Starting Ping", context.bot, update.message)
+    end_time = int(round(time() * 1000))
+    editMessage(f'{end_time - start_time} ms', reply)
 
+
+def log(update, context):
+    sendLogFile(context.bot, update.message)
+    
+    
 def start(update, context):
     fname = update.message.chat.first_name
     update.message.reply_text(
@@ -21,7 +92,7 @@ def help(update, context):
 
 
 def list(update, context):
-    files = os.listdir("/app/telegramMusic")
+    files = os.listdir("/app/bot/telegramMusic")
     string = ""
     for l in files:
         if l.endswith(".mp3"):
@@ -83,17 +154,27 @@ def main():
     dp.add_handler(CommandHandler("list", list))
 
     dp.add_handler(CommandHandler("contact", contact))
+    
+    dp.add_handler(CommandHandler("Restart", restart))
+    
+    dp.add_handler(CommandHandler("stats", stats))
+    
+    dp.add_handler(CommandHandler("ping", ping))
+    
+    dp.add_handler(CommandHandler("log", log))
 
     dp.add_handler(MessageHandler(Filters.text, download, run_async=True))
 
     dp.add_error_handler(error_handler)
 
-    logger.info("Loaded all handlers")
-
-    # updater.start_polling()
     updater.start_polling()
-    updater.idle()
+    
+    logger.info("Bot Started!")
+    
+    signal.signal(signal.SIGINT)
 
 
-if __name__ == "__main__":
-    main()
+app.start()
+main()
+
+main_loop.run_forever()
